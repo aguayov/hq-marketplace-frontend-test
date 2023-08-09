b00941cae462d2740972976d7b66be09ff1bcd1a
aguayov <69515250+aguayov@users.noreply.github.com>
2023-08-08 19:59:02 -0600

    add global styles sheet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
-import type { AppProps } from "next/app"
-import dynamic from "next/dynamic"
+import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import '../styles/globals.css';
 
 const MarketplaceContextProvider = dynamic(
-  () => import("context/MarketplaceContextProvider"),
+  () => import('context/MarketplaceContextProvider'),
   { ssr: false }
-)
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MarketplaceContextProvider>
       <Component {...pageProps} />
     </MarketplaceContextProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
